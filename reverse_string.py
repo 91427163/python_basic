18a1572998751cff457acac836e33082b3900cc0
91427163 <emokhtari7163@gmail.com>
2026-07-14 13:05:51 +0330

    Update reverse_string.py
    Add reverse string

--- reverse_string.py
+++ reverse_string.py
@@ -1,1 +1,18 @@
+#method one (reverse by slicing)
+def reverse_slicing(txt):
 
+    return txt[-1::-1]
+
+#method two (reverse by loop)
+def reverse_loop(txt):
+
+    rev_txt = ""
+    for char in range(len(txt)-1, -1,-1):
+        rev_txt += txt[char]
+    return rev_txt
+
+#Testing the Function
+txt = input("Please Enter Your Text : ")
+print(f"Original: {txt}")
+print(f"Slicing: {reverse_slicing(txt)}")
+print(f"Loop: {reverse_loop(txt)}")
